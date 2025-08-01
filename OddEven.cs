4936becf2eec93811f449c563c0155ed7f8ec20a
Naveen_02 <ny0919063@gmail.com>
2025-08-01 11:43:51 +0530

    Add files via upload

---
+++ OddEven.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven
+{
+    internal class OddEven
+    {
+        public void Oddeven(int n)
+        {
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+        public void display(int n)
+        {
+            Console.WriteLine($"The number is {n}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OddEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            OddEven oddEven = new OddEven();
+            oddEven.Oddeven(n);
+            oddEven.display(n);
+        }
+    }
+}
