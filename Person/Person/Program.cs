37f5e310ea42a944b7076952c9151ce07b9172c5
Naveen_02 <ny0919063@gmail.com>
2025-09-19 13:51:40 +0530

    Add files via upload

---
+++ Person/Person/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Person
+{
+    public class Program
+    {
+        public static void Person()
+        {
+            
+            Person p1 = new Person();
+            p1.ReadData();
+            p1.DisplayData();
+        }
+       public static void Main(string[] args)
+        {
+            Person(); 
+        }
+    }
+}
+
