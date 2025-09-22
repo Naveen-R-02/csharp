a0c15aaf72acb7aa2eedf7c7d7bd85643213953a
Naveen_02 <ny0919063@gmail.com>
2025-09-22 18:40:38 +0530

    Add files via upload

---
+++ GreekPerson/GreekPerson/GreekcClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreekPerson
+{
+    public class GreekClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+ "Enter name:");
+            String name = Console.
+ Readline();
+            Greek g = new Greek(name);
+            g.greek();
+
+        }
+    }
+}
---
+++ GreekPerson/GreekPerson/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GreekPerson
+{
+  public class Greek
+    {
+        String name;
+        public Greek()
+        {
+            name = " lisha ";
+        }
+        public Greek(string name)
+        {
+            this.name = name;
+        }
+        public void greek()
+        {
+            Console.WriteLine( 
+ "Good morning,{name}!");
+        }
+    }
+  
+            
+       
+    }
